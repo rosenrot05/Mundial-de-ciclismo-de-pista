f85e78ea8024065f9d00ff7f80b1016b58e3f301
Rosa Maria Peña <rosamariapca10@gmail.com>
2026-04-09 21:03:40 -0500

    Creacion del proyecto

---
+++ src/main/java/com/mycompany/ciclismopista/CiclismoPista.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ciclismopista;
+
+/**
+ *
+ * @author rosam
+ */
+public class CiclismoPista {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
