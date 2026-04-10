6e7b036a9cfe6f85fe42ca6c269680291e1f2f6d
Rosa Maria Peña <rosamariapca10@gmail.com>
2026-04-09 21:35:29 -0500

    Creacion de la subclase Velocista que hereda de Competidor

--- src/main/java/Modelo/Velocista.java
+++ src/main/java/Modelo/Velocista.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Modelo;
 
 /**
  *
  * @author rosam
  */
-public class Velocista {
+public class Velocista extends Competidor {
     
+        private double velocidadMaxima;
+
+    public Velocista(String nombre, int edad, String pais, int ranking, double estatura, double peso, double velocidadMaxima) {
+        super(nombre, edad, pais, ranking, estatura, peso);
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Tipo: Velocista, Velocidad: " + velocidadMaxima;
+    }
 }
