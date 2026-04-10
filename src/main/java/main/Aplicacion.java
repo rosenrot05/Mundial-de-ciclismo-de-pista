dfcbc513694a92e6d47531265acfc8291cc2f50c
Rosa Maria Peña <rosamariapca10@gmail.com>
2026-04-09 22:03:15 -0500

    Implementacion de la clase principal Aplicacion con integracion del sistema

--- src/main/java/main/Aplicacion.java
+++ src/main/java/main/Aplicacion.java
@@ -1,6 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package main;
 
@@ -8,9 +5,46 @@ package main;
  *
  * @author rosam
  */
+
+import Controlador.ControladorCompetencia;
+import Modelo.*;
+import Vista.VistaCompetencia;
+
 public class Aplicacion {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+                VistaCompetencia vista = new VistaCompetencia();
+        int cantidad = vista.leerCantidad();
+
+        ControladorCompetencia controlador = new ControladorCompetencia(cantidad);
+
+        for (int i = 0; i < cantidad; i++) {
+
+            String nombre = vista.leerNombre();
+            int edad = vista.leerEdad();
+            String pais = vista.leerPais();
+            int ranking = vista.leerRanking();
+            double estatura = vista.leerEstatura();
+            double peso = vista.leerPeso();
+
+            int tipo = vista.tipoCompetidor();
+            double extra = vista.datoExtra(tipo);
+
+            Competidor c;
+
+            if (tipo == 1) {
+                c = new Velocista(nombre, edad, pais, ranking, estatura, peso, extra);
+            } else {
+                c = new Resistencia(nombre, edad, pais, ranking, estatura, peso, extra);
+            }
+
+            // aplicar logica (sobrecarga)
+            c.actualizarRanking(5);
+
+            controlador.agregarCompetidor(c);
+        }
+
+        vista.mostrar(controlador.mostrarCompetidores());
     }
+    
 }
