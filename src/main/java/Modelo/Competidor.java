6ec3d71ff4a85b330aa47449139205989481a200
Rosa Maria Peña <rosamariapca10@gmail.com>
2026-04-09 21:29:29 -0500

    Creacion de la clase Competidor con atributos, constructores, getters y setters

--- src/main/java/Modelo/Competidor.java
+++ src/main/java/Modelo/Competidor.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Modelo;
 
 /**
@@ -10,4 +7,53 @@ package Modelo;
  */
 public class Competidor {
     
+    protected String nombre;
+    protected int edad;
+    protected String pais;
+    protected int ranking;
+    protected double estatura;
+    protected double peso;
+
+    public Competidor(String nombre, int edad, String pais, int ranking, double estatura, double peso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.pais = pais;
+        this.ranking = ranking;
+        this.estatura = estatura;
+        this.peso = peso;
+    }
+
+    // GETTERS Y SETTERS
+    public String getNombre() { return nombre; }
+    public int getRanking() { return ranking; }
+
+    //Metodo para actualizar ranking
+    public void actualizarRanking(int puntos) {
+        ranking = ranking - puntos;
+    }
+
+    // Sobrecarga 
+    public void actualizarRanking(int puntos, int penalizacion) {
+        ranking = ranking - (puntos - penalizacion);
+    }
+
+    // Metodo con estructura anidada 
+    public void actualizarRanking(double puntos, boolean bonus) {
+        if (bonus) {
+            if (puntos > 10) {
+                ranking -= puntos * 2;
+            } else {
+                ranking -= puntos;
+            }
+        } else {
+            ranking -= puntos;
+        }
+    }
+
+    // Sobreescritura sugerida
+    @Override
+    public String toString() {
+        return "Nombre: " + nombre + ", Pais: " + pais + ", Ranking: " + ranking;
+    }
 }
+
